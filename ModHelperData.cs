16ba156fbe3ccc054a597069fbec7f0a3f85ab31
Porygon-Z208 <65074681+Porygon-Z208@users.noreply.github.com>
2023-10-16 20:14:03 -0400

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace BTD7Sniper;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "39.0";
-    public const string Version = "1.0.0";
+    public const string Version = "0.9.0";
     public const string Name = "BTD7 Sniper";
 
     public const string Description = "Adds a 4th path and 6th tiers to the sniper, also changes the crosspathing rules to tier 6 in one path, and tier 2 in 2 others, except you can get a tier 3 in one path if you have a tier 6 on that tower. (Requires Paths++ and Ultimate Crosspathing)";
